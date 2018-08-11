3fb227ba35d0daa8c2337cf8761cf68a752a973d
steveyout <34503856+steveyout@users.noreply.github.com>
2018-08-11 13:45:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 
-token='628778240:AAFoknZwg0rMPSp18yX3_bubVVjOBKQ4Ta8'
+token='628778240:AAEss1X8lvozbVBWZaAekXTYInrdKj_EJe8'
 payeer_account='P1002894412'
 payeer_api_pass='hello'
 payeer_api_id=618058477
